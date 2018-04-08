149c646b59559d4578f2d43a43d1d960479d1c08
shuangfanguang <695166116@qq.com>
2018-04-09 00:34:44 +0800

    update Version

---
+++ ShootR/Startup/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Security.Claims;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using ShootR.Authentication;
+using TweetSharp;
+
+namespace ShootR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            // Disable keep alive, no need
+            GlobalHost.Configuration.DisconnectTimeout = TimeSpan.FromMinutes(3);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ShootR",
+                Provider = new ShootRAuthenticationProvider()
+            });
+        }
+    }
+}
